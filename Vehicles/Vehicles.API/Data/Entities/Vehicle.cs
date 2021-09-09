2b4ebd29da8f8b863c1eb60881ebb4f6a5d08212
Enrique Barajas C <enbace74@gmail.com>
2021-09-09 00:11:45 -0500

    Add API for access movile

--- Vehicles/Vehicles.API/Data/Entities/Vehicle.cs
+++ Vehicles/Vehicles.API/Data/Entities/Vehicle.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 
 namespace Vehicles.API.Data.Entities
 {
@@ -40,6 +41,7 @@ namespace Vehicles.API.Data.Entities
 
         [Display(Name = "Propietario")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [JsonIgnore]
         public User User { get; set; }
 
         [Display(Name = "Observación")]
